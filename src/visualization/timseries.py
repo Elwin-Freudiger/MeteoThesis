e7d13efd39737b8a4d12fff6b92819fbc254407f
Elwin Freudiger <35497600+Elwin-Freudiger@users.noreply.github.com>
2025-02-26 16:07:38 +0100

    Added timeseries visualization

--- src/visualization/timseries.py
+++ src/visualization/timseries.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def main():
+    df = pd.read_csv("data/clean/station_precipitation.csv")
+    df = df[df['station']=="TAE"]
+    
+    plt.plot(df[['precip']])
+    plt.savefig("report/figures/timeserie.png")
+
 
 if __name__ == "__main__":
-    None
-
-    
+    main()
